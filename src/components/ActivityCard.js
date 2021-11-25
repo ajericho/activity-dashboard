450941b215d746c9f97bbdc5285e1ec6e6355e9d
Amanda Jericho <ajericho@gmail.com>
2021-11-25 15:27:22 -0500

    desktop styles

--- src/components/ActivityCard.js
+++ src/components/ActivityCard.js
@@ -4,13 +4,12 @@ export default function ActivityCard({ timeframe, activity }) {
 
     const t = timeframe;
 
-    function createId(x)
-{
-    return x
-        .toLowerCase()
-        .replace(/ /g,'-')
-        ;
-}
+    function createId(x) {
+        return x
+            .toLowerCase()
+            .replace(/ /g, '-')
+            ;
+    }
 
 
 
@@ -20,7 +19,11 @@ export default function ActivityCard({ timeframe, activity }) {
             <div class="activity-main-box">
                 <div class="activity-title">
                     <h4>{activity.title}</h4>
-                    <div>...</div>
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="dots" viewBox="0 0 20 20" fill="currentColor">
+                            <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
+                        </svg>
+                    </div>
                 </div>
 
                 {activity.timeframes[t] ?
